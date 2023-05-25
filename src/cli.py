f03109a8994a471f9322d4c1f9d7fd80feb363ad
Parth Shandilya <24358501+ParthS007@users.noreply.github.com>
2023-05-25 15:31:35 +0200

    cli: add options for pid and subjects

--- src/cli.py
+++ src/cli.py
@@ -4,27 +4,37 @@
 import click
 
 from .api import make_api_request
-from .utils import get_query
+from .utils import get_full_query
 
 
 @click.command()
 @click.option(
+    "--pid",
+    type=click.STRING,
+    multiple=True,
+    help="The document pid. Example: w4rr3-d4920 ",
+)
+@click.option(
     "--created",
     type=click.STRING,
-    required=True,
     help='Date range for created filter in "YYYY-MM-DD:YYYY-MM-DD" format.',
+)
+@click.option(
+    "--subjects",
+    type=click.STRING,
+    multiple=True,
+    help="Subject and/or the scheme. Example: 005*:UDC, 65*",
 )
 @click.option(
     "--pub-year",
     type=click.STRING,
-    required=True,
     help='Year range for publication year filter in "YYYY:YYYY" format.',
 )
-def get_results(created, pub_year):
-    query = get_query(created, pub_year)
+def get_documents_to_notify(pid, created, subjects, pub_year):
+    query = get_full_query(pid, created, subjects, pub_year)
     filtered_docs = make_api_request(query)
     click.echo(filtered_docs)
 
 
 if __name__ == "__main__":
-    get_results()
+    get_documents_to_notify()
